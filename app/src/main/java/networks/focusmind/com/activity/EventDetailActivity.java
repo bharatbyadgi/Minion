27a35d35642eae594c4204a9d99e0eb8cdd0d230
bharat <bharat.byadgi@thetechchefs.in>
2017-03-18 14:29:50 +0530

    initial commit

---
+++ app/src/main/java/networks/focusmind/com/activity/EventDetailActivity.java
@@ -0,0 +1,15 @@
+package networks.focusmind.com.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import networks.focusmind.com.minion.R;
+
+public class EventDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_detail);
+    }
+}
